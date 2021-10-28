4c8c57309428734b9edd5e92d45b8d1e8e1a378c
Aleksandr <sssalex06@gmail.com>
2021-10-28 16:33:43 +0300

    add future highlight link in navbar

--- js/main.js
+++ js/main.js
@@ -301,7 +301,18 @@ $(document).ready(function(){
         $('#mc_embed_signup').find('form').ajaxChimp();
     });      
 });
-/*  */
+/* =============================== */
+/* highLight active link in navBar */
+/* =============================== */
+let links = document.querySelectorAll('.nav-menu > li > a')
+links.forEach(item => {
+  (item.href === window.location.href )
+    ? item.parentNode.classList.add('menu-active')
+    : item.parentNode.classList.remove('menu-active')
+})
+/* =============================== */
+/* =========== pop-up ============ */
+/* =============================== */
 const BTNs = document.querySelectorAll('.btn-popup')
 const popUpBlock = `
   <div class="pop-up">
@@ -326,39 +337,52 @@ function hidePopup() {
 } 
 
 
+/* 
+1. http://intuit.ru:80/r/dir/page?search#mark
+2. window.location.href = [url]http://intuit.ru:80/r/dir/page?search#mark[/url]
+3. window.location.protocol = http;
+4. window.location.hostname = intuit.ru;
+5. window.location.host = intuit.ru:80;
+6. window.location.port = 80
+7. window.location.pathname = /r/dir/;
+8. window.location.search = search;
+9. window.location.hash = mark;
+======
+window.location.protocol = http: ,
+window.location.pathname = /r/dir/page ,
+window.location.search = ?search ,
+window.location.hash = #mark
+*/
 
 
 /*
-    //  Start Google map 
-
-            // When the window has finished loading create our google map below
-
+    //  Start Google map  // When the window has finished loading create our google map below
 if(document.getElementById("map")){
-      google.maps.event.addDomListener(window, 'load', init);
-      function init() {
-          // Basic options for a simple Google Map
-          // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-          var mapOptions = {
-              // How zoomed in you want the map to start at (always required)
-              zoom: 11,
-              // The latitude and longitude to center the map (always required)
-              center: new google.maps.LatLng(50.454, 30.523), // New York
-              // How you would like to style the map. 
-              // This is where you would paste any style found on Snazzy Maps.
-              styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}]
-          };
-          // Get the HTML DOM element that will contain your map 
-          // We are using a div with id="map" seen below in the <body>
-          var mapElement = document.getElementById('map');
-          // Create the Google Map using our element and options defined above
-          var map = new google.maps.Map(mapElement, mapOptions);
-          // Let's also add a marker while we're at it
-          var marker = new google.maps.Marker({
-              position: new google.maps.LatLng(40.6700, -73.9400),
-              map: map,
-              title: 'Snazzy!'
-          });
-      }
-    }
- */
+  google.maps.event.addDomListener(window, 'load', init);
+  function init() {
+      // Basic options for a simple Google Map
+      // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+      var mapOptions = {
+          // How zoomed in you want the map to start at (always required)
+          zoom: 11,
+          // The latitude and longitude to center the map (always required)
+          center: new google.maps.LatLng(50.454, 30.523), // New York
+          // How you would like to style the map. 
+          // This is where you would paste any style found on Snazzy Maps.
+          styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}]
+      };
+      // Get the HTML DOM element that will contain your map 
+      // We are using a div with id="map" seen below in the <body>
+      var mapElement = document.getElementById('map');
+      // Create the Google Map using our element and options defined above
+      var map = new google.maps.Map(mapElement, mapOptions);
+      // Let's also add a marker while we're at it
+      var marker = new google.maps.Marker({
+          position: new google.maps.LatLng(40.6700, -73.9400),
+          map: map,
+          title: 'Snazzy!'
+      });
+  }
+}
+*/
 
